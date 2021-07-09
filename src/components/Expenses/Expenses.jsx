f72c21689d8738d6759137372208dd57acec16ea
Gauri Mhaiskar <gauri.mhaiskar@gmail.com>
2021-07-08 20:41:31 -0400

    added a filter component and passed its value up

--- src/components/Expenses/Expenses.jsx
+++ src/components/Expenses/Expenses.jsx
@@ -1,13 +1,24 @@
 import ExpenseItem from "../ExpenseItem/ExpenseItem";
 import Card from "../Card/Card";
+import ExpensesFilter from "./ExpensesFilter";
 import "./Expenses.css";
+import { useState } from "react";
+
 function Expenses(props) {
+  const [filteredYear, setFilteredYear] = useState("2020");
+  const filterHandler = (year) => {
+    // console.log(year);
+    setFilteredYear(year);
+  };
   return (
-    <Card className="expenses">
-      {props.expenses.map((expense) => {
-        return <ExpenseItem key={expense.id} expense={expense} />;
-      })}
-    </Card>
+    <div>
+      <ExpensesFilter onFilter={filterHandler} selected={filteredYear} />
+      <Card className="expenses">
+        {props.expenses.map((expense) => {
+          return <ExpenseItem key={expense.id} expense={expense} />;
+        })}
+      </Card>
+    </div>
   );
 }
 export default Expenses;
